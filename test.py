b1b9cce204ded8b1f360e0193454d7e9f5855059
Anton Agestam <msn@antonagestam.se>
2014-03-01 14:18:46 +0100

    changes

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 #!/usr/local/bin/python
 
-from squash.compiler import squash
+from squash import squash
 
 result = squash("""
 .noselect {
   wtf: yes;
+  color: green;
 }
 
 .noselect, .ellipsify {
@@ -20,10 +21,11 @@ result = squash("""
   text-overflow: ellipsis;
   white-space: nowrap;
   overflow: hidden;
+  color: green;
 }""")
 
 print result
 
-assert result == (""".noselect,.ellipsify {-webkit-user-select: none;-moz-user-select: none;-webkit-touch-callout: none;-khtml-user-select: none;-ms-user-select: none;user-select: none;}
+assert result == (""".noselect,.ellipsify {-webkit-user-select: none;-moz-user-select: none;-webkit-touch-callout: none;-khtml-user-select: none;-ms-user-select: none;user-select: none;color: green;}
 .ellipsify {white-space: nowrap;overflow: hidden;text-overflow: ellipsis;}
 .noselect {wtf: yes;}""")
